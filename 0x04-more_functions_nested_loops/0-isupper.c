491a49fdb2aedc18319af541135b30e9bdbd0f51
Mtende265 <kumwendamtende@gmail.com>
2023-03-23 00:40:52 -0700

    pushing

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ */
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* mul - multiplies two integers
+*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
