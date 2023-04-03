3e5a70e40135c1476d28b4a855f831578c72eb8d
Mtende265 <kumwendamtende@gmail.com>
2023-04-03 07:02:46 -0700

    1&2

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ *
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: with the constant byte @b
+ *
+ * @b: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
+ */
+
 /*written by Codemonger*/
 
 char *_memset(char *s, char b, unsigned int a)
--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,17 @@
 #include "main.h"
 
+/**
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
+ */
 char *_memcpy(char *src, char *dest, unsigned int a)
 {
 	unsigned int i = 0;
